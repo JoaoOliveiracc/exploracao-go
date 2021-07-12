9b63a5f1b4edfa6aa7a9e7a709445cbd3cb9603e
João Monteiro <ccjoao_monteiro@outlook.com>
2021-07-12 12:31:01 -0300

    slice

---
+++ arrayslicemap/slice/slice.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	a1 := [3]int{1, 2, 3} // array
+	s1 := []int{1, 2, 3}  // slice
+	fmt.Println(reflect.TypeOf(a1), reflect.TypeOf(s1))
+
+	a2 := [5]int{1, 2, 3, 4, 5}
+
+	// Slice não é um array! Slice define um pedaço de um array.
+	s2 := a2[1:3]
+	fmt.Println(a2, s2)
+
+	s3 := a2[:2] // novo slice, apontando para o mesmo array a2
+	fmt.Println(a2, s3)
+
+	// pode-se imaginar um slice como: tamanho de um ponteiro para um elemento de um array
+	s4 := s2[:1]
+	fmt.Println(s2, s4)
+}
