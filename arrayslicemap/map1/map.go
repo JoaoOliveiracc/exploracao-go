d85abce9a4b523a0b55a83e290c69ebff1b9503f
João Monteiro <ccjoao_monteiro@outlook.com>
2021-07-14 12:10:31 -0300

    map

---
+++ arrayslicemap/map1/map.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var aprovados map[int]string
+	// mapas devem ser inicializados
+	aprovados := make(map[int]string)
+
+	aprovados[12345678978] = "Maria"
+	aprovados[98765432112] = "Pedro"
+	aprovados[65432198700] = "Ana"
+
+	fmt.Println(aprovados)
+
+	for cpf, nome := range aprovados {
+		fmt.Printf("%s (CPF: %d)\n", nome, cpf)
+	}
+
+	fmt.Println(aprovados[65432198700])
+	delete(aprovados, 65432198700)
+	fmt.Println(aprovados[65432198700])
+}
